97308c4354f733ff1eecae6dc4d9032a326bf917
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-17 20:04:32 -0500

    add components folder

---
+++ src/components/items/items.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class items extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default items;
