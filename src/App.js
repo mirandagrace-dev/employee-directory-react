e9c5dd701fe41d83b73c1f3d008021491dc0fb46
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-02-17 21:34:02 -0500

    added table styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Items from "./components/items/Items"
+import Items from "./components/items/Items";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
 			<Items />
 		</div>
 	);
